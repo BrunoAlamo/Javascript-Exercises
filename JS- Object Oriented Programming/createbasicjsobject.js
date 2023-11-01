ed820740ea7023cc550c81b600f2b046442e314e
BrunoAlamo <147200493+BrunoAlamo@users.noreply.github.com>
2023-11-01 13:50:39 -0300

    Add files via upload

---
+++ JS- Object Oriented Programming/createbasicjsobject.js
@@ -0,0 +1,10 @@
+//Create a basic Javascript Object
+
+//Create a dog object with name and numLegs properties, and set them to a string and a number, respectively
+
+let dog = {
+    name: "zammy",
+    numLegs: 4
+};
+
+console.log(dog.name);
