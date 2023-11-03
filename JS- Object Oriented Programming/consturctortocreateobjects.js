121e32adb19fc34657dc8b9ce72b72d509e79904
BrunoAlamo <147200493+BrunoAlamo@users.noreply.github.com>
2023-11-03 14:23:51 -0300

    Add files via upload

---
+++ JS- Object Oriented Programming/consturctortocreateobjects.js
@@ -0,0 +1,11 @@
+//Use the dog constructor to create a new instante of Dog, assigning it to a variable hound
+
+function dog() {
+    this.name = "zammy";
+    this.color = "black and white";
+    this.numLegs = 4;
+}
+
+let hound = new dog();
+
+console.log(hound);
