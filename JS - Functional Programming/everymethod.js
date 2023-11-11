f3594791611f12608e1ce55af830608f79951ae1
BrunoAlamo <147200493+BrunoAlamo@users.noreply.github.com>
2023-11-11 19:02:27 -0300

    Add files via upload

---
+++ JS - Functional Programming/everymethod.js
@@ -0,0 +1,9 @@
+//Use the every Method to Check that Every Element in an Array Meets a Criteria
+
+function checkPositive(arr) {
+    return arr.every(function(value) {
+       return value > 0;
+     });
+   }
+   
+   console.log(checkPositive([1, 2, 3, -4, 5]));
