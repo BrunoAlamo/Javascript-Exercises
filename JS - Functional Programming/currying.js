09a5276a87395f4a3356773c744de22f48600fbf
BrunoAlamo <147200493+BrunoAlamo@users.noreply.github.com>
2023-11-12 03:00:38 -0300

    Add files via upload

---
+++ JS - Functional Programming/currying.js
@@ -0,0 +1,11 @@
+//Introduction to Currying and Partial Application
+
+function add(x) {
+    return function(y){
+        return function(z){
+            return x + y + z;
+        }
+    }
+}
+
+console.log(add(10)(20)(30));
