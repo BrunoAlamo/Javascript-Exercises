e8e2d36d99f41b253476f18dd5f6599310ca451e
BrunoAlamo <147200493+BrunoAlamo@users.noreply.github.com>
2025-02-06 20:48:32 -0300

    Create Pokemon.js

---
+++ Pokemon.js
@@ -0,0 +1,29 @@
+const searchInput = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-button");
+const pokemonName = document.getElementById("pokemon-name");
+const pokemonId = document.getElementById("pokemon-id");
+const weight = document.getElementById("weight");
+const height = document.getElementById("height");
+const types = document.getElementById("types");
+const hp = document.getElementById("hp");
+const attack = document.getElementById("attack");
+const defense = document.getElementById("defense");
+const specialAttack = document.getElementById("special-attack");
+const specialDefense = document.getElementById("special-defense");
+const speed = document.getElementById("speed");
+const sprite = document.getElementById("sprite");
+
+const fetchData = async () => {
+  try{
+    const pokemonNameOrId = searchInput.value.toLowerCase();
+    const res = await fetch(`https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${pokemonNameOrId}`);
+    const data = await res.json();
+  } catch(err) {
+    alert("Pokémon not found");
+    console.log(err);
+  }
+}
+
+const setPokemonInfo = data => {
+  const { name, id, weight, height, types, sprites, stats } = data;
+}
