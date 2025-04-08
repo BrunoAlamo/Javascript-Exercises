e24c6707d81a36fa480f7e4fcfdb39c81553bff7
BrunoAlamo <147200493+BrunoAlamo@users.noreply.github.com>
2025-04-08 11:46:35 -0300

    Add files via upload

---
+++ Code Wars/findcalculationstype.js
@@ -0,0 +1,14 @@
+function calcType(a, b, res) {
+    if(a + b === res){
+      return "addition";
+    } 
+    if(a - b === res){
+      return "subtraction";
+    }
+    if(a * b === res){
+      return "multiplication";
+    }
+    if(a / b === res) {
+      return "division";
+    }
+  }
